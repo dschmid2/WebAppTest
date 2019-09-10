2f7b5d1b543ff314b2dc041fd53cc9b33dbfea0c
Daniel Schmid <schmid.daniel@kpt.ch>
2019-09-10 13:57:10 +0200

    Anpassung Kontaktseite

--- WebApplicationTest/Controllers/HomeController.cs
+++ WebApplicationTest/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace WebApplicationTest.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Die Kontaktseite";
 
             return View();
         }
